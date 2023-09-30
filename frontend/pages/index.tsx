42e80f4169cce389590a2c17c44055e96dff06b1
Ryan Gao <67180069+toblrne@users.noreply.github.com>
2023-09-29 23:33:23 -0700

    pages back

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Navbar from "../components/Navbar";
+
+
+const Home: NextPage = () => {
+  return (
+    <Box minH="100vh" position="relative">
+      <Head>
+        Palette
+      </Head>
+      <Navbar />
+      <Box>
+        photos stub
+      </Box>
+    </Box>
+  )
+}
+
+export default Home
+
